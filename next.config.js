362ce53190a2b47626281114bda46650c9d876e0
Drew Bredvick <dbredvick@gmail.com>
2020-04-06 17:33:03 -0500

    task: fix email

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withSass = require("@zeit/next-sass");
+const dotenv = require("dotenv");
+dotenv.config();
 
-let nextConfig = withSass();
+let nextConfig = withSass({
+  env: {
+    CONVERT_KIT_API_KEY: process.env.CONVERT_KIT_API_KEY,
+  },
+});
 
 // You can pass nextConfig to another plugin, for example:
 //nextConfig = withMdx(nextConfig)
